65561f6998c661eeab3ae1ee15f101f0d4130086
Jason R. Carrete <jasoncarrete5@gmail.com>
2020-10-03 00:36:28 -0400

    Add some comments to repl.c

--- ftpclient/repl.c
+++ ftpclient/repl.c
@@ -1,1 +1,17 @@
+/*
+ * CS472 HW 2
+ * Jason R. Carrete
+ * repl.c
+ *
+ * This module implements the Read-Eval-Print-Loop (REPL) for the application.
+ * The REPL is responsible for processing user input and displaying output in
+ * response.
+ */
+
 #include "repl.h"
+
+/*
+ * TODO
+ * MUST implement USER, PASS, CWD, QUIT, PASV, EPSV, PORT, EPRT, RETR, STOR,
+ * PWD, SYST, LIST, and HELP commands
+ */
