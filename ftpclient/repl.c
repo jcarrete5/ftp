ae7935a164f4a615a9537d471984530b8182d9bf
Jason R. Carrete <jasoncarrete5@gmail.com>
2020-10-06 13:57:10 -0400

    Redesign REPL to use vectors
    Removed usage of long jumps

--- ftpclient/repl.c
+++ ftpclient/repl.c
@@ -14,76 +14,54 @@
 #include <stdbool.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <setjmp.h>
 #include <unistd.h>
 
 #include "repl.h"
 #include "log.h"
+#include "vector.h"
 #include "ftp.h"
-
-/* Error jump buffer for error handling. */
-jmp_buf err_jmp_buf;
 
 /* Socket file descriptor for the user-PI. */
 static int sockpi;
 
 static const size_t MIN_STR_BUFFER_SIZE = (size_t) 1024;
 
+/* Read user input from stdin into the vector. */
+void get_input_str(struct vector *str) {
+    int ch;
+    while ((ch=getchar()) != EOF && ch != '\n') {
+        vector_append(str, (char) ch);
+    }
+    vector_append(str, '\0');
+}
+
 /* Cleanup resources used by the repl on exit. */
 static void cleanup(void) {
+    loginfo("Closing user-PI socket");
     close(sockpi);
 }
 
-/* Go through authentication sequence with the remove server-PI. */
-static void login_with_credentials(void) {
-    switch (setjmp(err_jmp_buf)) {
-        case FATAL_ERROR:
-            logerr("Aborting");
-            puts("A fatal error occured. See log for details");
-            exit(EXIT_FAILURE);
-            break;
-        case ILLEGAL_ARG:
-            puts("Invalid argument. See log for details");
-            break;
-    }
+/* Go through authentication sequence with the remote server-PI. */
+static enum reply_code login_with_credentials(void) {
+    struct vector str;
+    vector_create(&str, 64, 2);
     printf("Username: ");
-    char username[4096];
-    scanf(" %4095s", username);
-    ftp_USER(sockpi, username);
-    printf("Password: ");
-    char password[4096];
-    scanf(" %4095s", password);
-    ftp_PASS(sockpi, password);
-}
-
-/*
- * Read user input from stdin and return the resulting string. The returned
- * string is dynamically allocated and **must** be freed after use.
- */
-const char *get_input_str(void) {
-    size_t size = MIN_STR_BUFFER_SIZE;
-    char *buf = calloc(size, sizeof(char));
-    if (!buf) {
-        logerr("Failed to allocate memory for user input");
-        longjmp(err_jmp_buf, FATAL_ERROR);
+    get_input_str(&str);
+    enum reply_code reply = ftp_USER(sockpi, str.arr);
+    if (ftp_pos_completion(reply)) {
+        puts("Username OK");
+    } else if (ftp_pos_intermediate(reply)) {
+        puts("Username OK");
+        printf("Password: ");
+        str.size = 0;  /* Reset array */
+        get_input_str(&str);
+        reply = ftp_PASS(sockpi, str.arr);
+        if (ftp_pos_completion(reply)) {
+            puts("Password OK");
+        } /* We don't need to implement ACCT as per requirements */
     }
-    size_t i = 0;
-    char ch;
-    while ((ch=getchar()) != EOF && ch != '\n') {
-        buf[i++] = ch;
-        /* +1 because we need to leave room for '\0' */
-        if (i + 1 == size) {
-            void *newptr = reallocarray(buf, size+=64, sizeof *buf);
-            if (newptr) {
-                buf = newptr;
-            } else {
-                logerr("Failed to reallocate memory for user input");
-                longjmp(err_jmp_buf, FATAL_ERROR);
-            }
-        }
-    }
-    buf[size - 1] = '\0';
-    return buf;
+    vector_free(&str);
+    return reply;
 }
 
 /* Start the REPL for the user-PI. */
@@ -92,12 +70,26 @@ void repl(const int sockfd) {
     if (atexit(cleanup)) {
         logwarn("Socket for user-PI will not be cleaned up on exit");
     }
-    /* TODO: Wait for reply from server */
-    login_with_credentials();
-    puts(FTPC_EXE_NAME" "FTPC_VERSION);
+    enum reply_code reply;
+    do {
+        reply = wait_for_reply(sockpi);
+        if (reply == FTP_SERVER_NA) {
+            puts("Server not currently available");
+            return;
+        }
+    } while (reply != FTP_SERVER_READY);
+    puts("Server is ready");
+    do {
+        reply = login_with_credentials();
+        if (!ftp_pos_completion(reply)) {
+            puts("Invalid login credentials");
+        }
+    } while (!ftp_pos_completion(reply));
+    struct vector in;
+    vector_create(&in, 64, 2);
     while (true) {
         printf("> ");
-        char buf[4096];
-        scanf(" %4095s", buf);
+        get_input_str(&in);
     }
+    vector_free(&in);
 }
--- ftpclient/repl.h
+++ ftpclient/repl.h
@@ -9,11 +9,6 @@
 #ifndef FTPC_REPL_H
 #define FTPC_REPL_H
 
-#include <setjmp.h>
-
-/* Error jump buffer for error handling. */
-extern jmp_buf err_jmp_buf;
-
 /* Start the REPL for the user-PI. */
 void repl(int sockfd);
 
